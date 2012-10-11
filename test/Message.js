c94ad637d8f720e5a1feb826580455c7ef5e1259
Eric Lee <eric.lee@exacttarget.com>
2012-10-11 13:07:10 -0700

    fix leak in Message ctor, more test coverage on obj construction

--- test/Message.js
+++ test/Message.js
@@ -1,10 +1,4 @@
-var should = require('should');
-_ = require('underscore');
-var binary = require('binary');
-var BufferMaker = require('buffermaker');
-var Message = require('../index').Message;
-
-describe("Message", function(){
+var should = require('should'); _ = require('underscore'); var binary = require('binary'); var BufferMaker = require('buffermaker'); var Message = require('../index').Message; describe("Message", function(){
   beforeEach(function(){
     this.message = new Message('');
   });
@@ -21,22 +15,22 @@ describe("Message", function(){
 
     it("should set a default value of one", function(){
       this.message.magic.should
-            .equal(this.message.MAGIC_IDENTIFIER_DEFAULT);
+      .equal(this.message.MAGIC_IDENTIFIER_DEFAULT);
     });
 
     it("should allow to set a custom magic number", function(){
-        this.message = new Message("ale", 0, 1, 0);
-        this.message.magic.should.equal(1);
+      this.message = new Message("ale", 0, 1, 0);
+      this.message.magic.should.equal(1);
     });
 
     it("should have a default compression type of 0", function(){
-        this.message = new Message("ale", 0);
-        this.message.compression.should.equal(0);
+      this.message = new Message("ale", 0);
+      this.message.compression.should.equal(0);
     });
 
     it("should allow to set a compression type", function(){
-        this.message = new Message("ale", 0, 1, 2);
-        this.message.compression.should.equal(2);
+      this.message = new Message("ale", 0, 1, 2);
+      this.message.compression.should.equal(2);
     });
 
     it("should throw an error if payload isn't set", function(){
@@ -45,6 +39,16 @@ describe("Message", function(){
         should.fail("expected exception was not raised");
       } catch(ex){
         ex.should.equal('payload is a required argument');
+      }
+    });
+
+    it("should throw an exception for an invalid compression type", function(){
+      try {
+        var INVALID_COMPRESSION_TYPE = 99;
+        this.message = Message('payload', 0, 1, INVALID_COMPRESSION_TYPE);
+        should.fail('ctor should not allow Messages to be constructed with bad compression types');
+      } catch(ex) {
+        true.should.equal(ex.toString().search('InvalidCompressionType:') > -1);
       }
     });
 
@@ -120,11 +124,11 @@ describe("Message", function(){
 
     it("should parse a message when magic is 0 (kafka <= 0.6)", function(){
       var bytes = new BufferMaker()
-                .UInt32BE(8) //size
-                .UInt8(0) //magic
-                .UInt32BE(1120192889)//checksum
-                .string("ale")
-                .make();
+      .UInt32BE(8) //size
+      .UInt8(0) //magic
+      .UInt32BE(1120192889)//checksum
+      .string("ale")
+      .make();
       var message = Message.fromBytes(bytes);
       message.magic.should.equal(0);
       message.checksum.should.equal(1120192889);
@@ -134,12 +138,12 @@ describe("Message", function(){
 
     it("should parse a message when magic is 1  (kafka >= 0.7)", function(){
       var bytes = new BufferMaker()
-                .UInt32BE(9) //size
-                .UInt8(1) // magic
-                .UInt8(0) // compression
-                .UInt32BE(1120192889) // checksum
-                .string("ale")
-                .make();
+      .UInt32BE(9) //size
+      .UInt8(1) // magic
+      .UInt8(0) // compression
+      .UInt32BE(1120192889) // checksum
+      .string("ale")
+      .make();
       var message = Message.fromBytes(bytes);
       message.magic.should.equal(1);
       message.checksum.should.equal(1120192889);
@@ -147,88 +151,88 @@ describe("Message", function(){
       message.payload.toString().should.equal("ale", 'utf8');
     });
 
-     it("should raise an error if the magic number is not recognised", function(){
-       var bytes = new Buffer(13);
-       bytes.writeUInt32BE(8, 0);          // size
-       bytes.writeUInt8(2, 4);              // magic
-       bytes.writeUInt8(1, 5);              // compression
-       bytes.writeUInt32BE(755095536, 6);   // checksum
-       bytes.write("ale", 10);
-       try {
-         Message.fromBytes(bytes);
-       } catch(err) {
-         err.should.equal("Unsupported Kafka message version: 2"); 
-       }
-     });
+    it("should raise an error if the magic number is not recognised", function(){
+      var bytes = new Buffer(13);
+      bytes.writeUInt32BE(8, 0);          // size
+      bytes.writeUInt8(2, 4);              // magic
+      bytes.writeUInt8(1, 5);              // compression
+      bytes.writeUInt32BE(755095536, 6);   // checksum
+      bytes.write("ale", 10);
+      try {
+        Message.fromBytes(bytes);
+      } catch(err) {
+        err.should.equal("Unsupported Kafka message version: 2"); 
+      }
+    });
 
-     it("should raise an exception on an incomplete 0.6 message", function(){
+    it("should raise an exception on an incomplete 0.6 message", function(){
       var bytes = new BufferMaker()
-                .UInt32BE(8)
-                .UInt8(0)
-                .UInt32BE(1120192889)
-                .string("al")
-                .make();
+      .UInt32BE(8)
+      .UInt8(0)
+      .UInt32BE(1120192889)
+      .string("al")
+      .make();
 
-       try {
-         var message= Message.fromBytes(bytes);
-         should.fail("expected exception was not raised.");
-       } catch(ex){
-         ex.should.equal("incomplete message");
-       }
-     });
+      try {
+        var message= Message.fromBytes(bytes);
+        should.fail("expected exception was not raised.");
+      } catch(ex){
+        ex.should.equal("incomplete message");
+      }
+    });
 
-     it("should raise an exception on an incomplete 0.7 message", function(){
+    it("should raise an exception on an incomplete 0.7 message", function(){
       var bytes = new BufferMaker()
-                .UInt32BE(9)
-                .UInt8(1)
-                .UInt8(0)
-                .UInt32BE(1120192889)
-                .string("al")
-                .make();
+      .UInt32BE(9)
+      .UInt8(1)
+      .UInt8(0)
+      .UInt32BE(1120192889)
+      .string("al")
+      .make();
 
-       try {
-         var message= Message.fromBytes(bytes);
-         should.fail("expected exception was not raised.");
-       } catch(ex){
-         ex.should.equal("incomplete message");
-       }
-     });
+      try {
+        var message= Message.fromBytes(bytes);
+        should.fail("expected exception was not raised.");
+      } catch(ex){
+        ex.should.equal("incomplete message");
+      }
+    });
 
-     it("should raise an exception on an incomplete message (no body) ", function(){
-       var bytes = new BufferMaker(23)
-                         .UInt32BE(8)           // size
-                         .UInt8(1)              // magic
-                         .UInt8(0)              // compression
-                         .UInt32BE(755095536)   // checksum
-                         // skipping message body here
-                         .make();   
-       try {
-         var message= Message.fromBytes(bytes);
-         should.fail("expected exception was not raised.");
-       } catch(ex){
-         ex.should.equal("incomplete message");
-       }
-     });
+    it("should raise an exception on an incomplete message (no body) ", function(){
+      var bytes = new BufferMaker(23)
+      .UInt32BE(8)           // size
+      .UInt8(1)              // magic
+      .UInt8(0)              // compression
+      .UInt32BE(755095536)   // checksum
+      // skipping message body here
+      .make();   
+      try {
+        var message= Message.fromBytes(bytes);
+        should.fail("expected exception was not raised.");
+      } catch(ex){
+        ex.should.equal("incomplete message");
+      }
+    });
 
-     it("should read empty messages correctly", function(){ 
+    it("should read empty messages correctly", function(){ 
       var bytes = new BufferMaker()
-                .UInt32BE(6)
-                .UInt8(1)
-                .UInt8(0)
-                .UInt32BE(0)
-                .string("")
-                .make();
-       var message = Message.fromBytes(bytes);
-       message.payload.toString().should.equal("");
-     });
+      .UInt32BE(6)
+      .UInt8(1)
+      .UInt8(0)
+      .UInt32BE(0)
+      .string("")
+      .make();
+      var message = Message.fromBytes(bytes);
+      message.payload.toString().should.equal("");
+    });
 
-     it("should parse a gzip-compressed message", function() {
-       // compressed = 'H4sIAG0LI1AAA2NgYBBkZBB/9XN7YlJRYnJiCogCAH9lueQVAAAA'.unpack('m*').shift
-       // bytes = [45, 1, 1, 1303540914, compressed].pack('NCCNa*')
-       // message = Message.parse_from(bytes).messages.first
-       // message.should be_valid
-       // message.payload.should == 'abracadabra'
-     });
+    it("should parse a gzip-compressed message", function() {
+      // compressed = 'H4sIAG0LI1AAA2NgYBBkZBB/9XN7YlJRYnJiCogCAH9lueQVAAAA'.unpack('m*').shift
+      // bytes = [45, 1, 1, 1303540914, compressed].pack('NCCNa*')
+      // message = Message.parse_from(bytes).messages.first
+      // message.should be_valid
+      // message.payload.should == 'abracadabra'
+    });
 
 
   });
